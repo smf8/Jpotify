80b426be5377e1823717b197381250bae9841b8f
Zahra <46957388+amberbrown4@users.noreply.github.com>
2019-06-23 12:51:37 +0430

    added loginPanel

--- player/src/View/Main.java
+++ player/src/View/Main.java
@@ -16,17 +16,21 @@ public class Main {
     public static void main(String[] args) throws MalformedURLException {
         // Testing playback controlling
 
-//        DatabaseConnection connection = new DatabaseConnection("test");
-//        DatabaseHandler databaseHandler = new DatabaseHelper(connection.getConnection());
-//        ArrayList<Song> songsQueue  = databaseHandler.searchSong("gojira");
-//        playbackManager = new PlaybackManager(songsQueue);
+        DatabaseConnection connection = new DatabaseConnection("test");
+        DatabaseHandler databaseHandler = new DatabaseHelper(connection.getConnection());
+        ArrayList<Song> songsQueue  = databaseHandler.searchSong("gojira");
+        playbackManager = new PlaybackManager(songsQueue);
 //        MainFrame mainFrame = new MainFrame();
-        MainFrame frame = new MainFrame();
-         //       SongPanel songPanel = new SongPanel();
+     //   MainFrame frame = new MainFrame();
+//                SongPanel songPanel = new SongPanel();
 
            //     frame.add(songPanel,BorderLayout.CENTER);
 //                frame.pack();
 //                frame.setVisible(true);
-
+            JFrame frame = new JFrame();
+            LoginPanel loginPanel = new LoginPanel();
+            frame.add(loginPanel);
+            frame.pack();
+            frame.setVisible(true);
     }
 }
